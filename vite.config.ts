35361425f1b3b1ef8e00b1d3e085b7f7e51b4c43
Lucas Teixeira <tlucasmatheus@gmail.com>
2026-08-24 19:41:55 -0300

    refactor: remove Tailwind CSS and implement Vanilla CSS for SelectTeam

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     watch: {
       ignored: ['**/myenv/**', '**/venv/**']
